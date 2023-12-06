95a0890fef2a88b9f5b715acf8c9285d2d332d08
Rafsan <Rafsan@DESKTOP-1AEISB8>
2023-12-06 22:54:35 +0000

    first commit

--- NoteVerse/Form1.Designer.cs
+++ NoteVerse/Form1.Designer.cs
@@ -28,18 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel2 = new Panel();
+            voiceBtn = new Button();
+            saveBtn = new Button();
+            stopBtn = new Button();
+            noteSpace = new RichTextBox();
+            newBtn = new Button();
+            notePanel1 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(81, 187, 147);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(383, 1118);
+            panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(81, 187, 147);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(3, 241);
+            button3.Name = "button3";
+            button3.Padding = new Padding(16, 0, 0, 0);
+            button3.Size = new Size(380, 97);
+            button3.TabIndex = 3;
+            button3.Text = "              Quizes";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(81, 187, 147);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(0, 126);
+            button2.Name = "button2";
+            button2.Padding = new Padding(20, 0, 0, 0);
+            button2.Size = new Size(383, 97);
+            button2.TabIndex = 2;
+            button2.Text = "              To-Do";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Teal;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 23);
+            button1.Name = "button1";
+            button1.Padding = new Padding(20, 0, 0, 0);
+            button1.Size = new Size(383, 97);
+            button1.TabIndex = 1;
+            button1.Text = "              Note";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.Control;
+            panel2.Controls.Add(voiceBtn);
+            panel2.Controls.Add(saveBtn);
+            panel2.Controls.Add(stopBtn);
+            panel2.Controls.Add(noteSpace);
+            panel2.Dock = DockStyle.Right;
+            panel2.Location = new Point(898, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(884, 1118);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // voiceBtn
+            // 
+            voiceBtn.Image = (Image)resources.GetObject("voiceBtn.Image");
+            voiceBtn.Location = new Point(765, 6);
+            voiceBtn.Name = "voiceBtn";
+            voiceBtn.Size = new Size(116, 86);
+            voiceBtn.TabIndex = 0;
+            voiceBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            saveBtn.Image = (Image)resources.GetObject("saveBtn.Image");
+            saveBtn.Location = new Point(3, 6);
+            saveBtn.Name = "saveBtn";
+            saveBtn.Size = new Size(116, 86);
+            saveBtn.TabIndex = 1;
+            saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // stopBtn
+            // 
+            stopBtn.Image = (Image)resources.GetObject("stopBtn.Image");
+            stopBtn.Location = new Point(631, 6);
+            stopBtn.Name = "stopBtn";
+            stopBtn.Size = new Size(116, 86);
+            stopBtn.TabIndex = 2;
+            stopBtn.UseVisualStyleBackColor = true;
+            // 
+            // noteSpace
+            // 
+            noteSpace.BorderStyle = BorderStyle.FixedSingle;
+            noteSpace.Location = new Point(0, 98);
+            noteSpace.Name = "noteSpace";
+            noteSpace.Size = new Size(884, 1020);
+            noteSpace.TabIndex = 2;
+            noteSpace.Text = "";
+            noteSpace.TextChanged += richTextBox1_TextChanged;
+            // 
+            // newBtn
+            // 
+            newBtn.BackColor = Color.Tomato;
+            newBtn.Dock = DockStyle.Top;
+            newBtn.ForeColor = SystemColors.ButtonHighlight;
+            newBtn.Image = (Image)resources.GetObject("newBtn.Image");
+            newBtn.Location = new Point(383, 0);
+            newBtn.Name = "newBtn";
+            newBtn.Size = new Size(515, 92);
+            newBtn.TabIndex = 6;
+            newBtn.Text = "New";
+            newBtn.TextAlign = ContentAlignment.MiddleRight;
+            newBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+            newBtn.UseVisualStyleBackColor = false;
+            newBtn.Click += newBtn_Click_1;
+            // 
+            // notePanel1
+            // 
+            notePanel1.AutoScroll = true;
+            notePanel1.Dock = DockStyle.Bottom;
+            notePanel1.Location = new Point(383, 93);
+            notePanel1.Name = "notePanel1";
+            notePanel1.Size = new Size(515, 1025);
+            notePanel1.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1778, 1107);
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1782, 1118);
+            Controls.Add(newBtn);
+            Controls.Add(notePanel1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private RichTextBox noteSpace;
+        private Button voiceBtn;
+        private Button saveBtn;
+        private Button stopBtn;
+        private Button newBtn;
+        private Panel notePanel1;
     }
 }
--- NoteVerse/Form1.cs
+++ NoteVerse/Form1.cs
@@ -1,10 +1,308 @@
+using System;
+using System.Data;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+
+
 namespace NoteVerse
 {
     public partial class Form1 : Form
     {
+        private TextBox noteName;
+        private TextBox noteNameTextBox;
+        private string connectionString = "Server=localhost;Database=NoteDatabase;Uid=root;Pwd=evan12345;";
+
         public Form1()
         {
             InitializeComponent();
+
+            noteName = new TextBox();
+            noteNameTextBox = new TextBox();
+
+
+            // Wire up the saveBtn Click event
+            saveBtn.Click += saveBtn_Click;
+            newBtn.Click += newBtn_Click;
+
+            DisplayNoteNames();
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            // Prompt the user to enter a new note name
+            string newNoteName = Interaction.InputBox("Enter the new note name:", "New Note Name", "");
+
+            if (!string.IsNullOrEmpty(newNoteName))
+            {
+                // Set the new note name to the noteSpace RichTextBox or any other control you are using
+                noteSpace.Text = newNoteName;
+                // Optionally, clear the existing noteSpace content
+                noteSpace.Text = "";
+
+                // Optionally, save the new note to the database with an empty noteText
+                SaveToDatabase(newNoteName, "");
+                DisplayNoteNames();
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedNoteName = noteNameTextBox.Text;
+
+                // Save the text from noteSpace under the name of the selected noteName
+                SaveToDatabase(selectedNoteName, noteSpace.Text);
+
+                MessageBox.Show("Note saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DisplayNoteNames();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveToDatabase(string noteName, string noteText)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Check if the noteName already exists in the database
+                bool noteExists = NoteExistsInDatabase(noteName, connection);
+
+                if (noteExists)
+                {
+                    // Update the existing note
+                    using (MySqlCommand updateCmd = new MySqlCommand("UPDATE Notes SET NoteText = @NoteText WHERE NoteName = @NoteName", connection))
+                    {
+                        updateCmd.Parameters.AddWithValue("@NoteName", noteName);
+                        updateCmd.Parameters.AddWithValue("@NoteText", noteText);
+                        updateCmd.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    // Insert a new note if it doesn't exist
+                    using (MySqlCommand insertCmd = new MySqlCommand("INSERT INTO Notes (NoteName, NoteText) VALUES (@NoteName, @NoteText)", connection))
+                    {
+                        insertCmd.Parameters.AddWithValue("@NoteName", noteName);
+                        insertCmd.Parameters.AddWithValue("@NoteText", noteText);
+                        insertCmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+
+        private List<string> GetNoteNamesFromDatabase()
+        {
+            // Implement this method to retrieve note names from the database
+            List<string> noteNames = new List<string>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("SELECT NoteName FROM Notes", connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            noteNames.Add(reader["NoteName"].ToString());
+                        }
+                    }
+                }
+            }
+
+            return noteNames;
+        }
+
+        private string GetNoteTextFromDatabase(string noteName)
+        {
+            // Implement this method to retrieve note text from the database
+            string noteText = string.Empty;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("SELECT NoteText FROM Notes WHERE NoteName = @NoteName", connection))
+                {
+                    cmd.Parameters.AddWithValue("@NoteName", noteName);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            noteText = reader["NoteText"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return noteText;
+        }
+
+        private void DisplayNoteNames()
+        {
+
+            ClearNoteButtons();
+
+            List<string> noteNames = GetNoteNamesFromDatabase();
+
+            Panel notePanel = new Panel();
+            notePanel.AutoScroll = true;
+            notePanel.Location = new Point(6, 23); // Adjust the location based on your layout
+            notePanel.Size = new Size(500, 400); // Adjust the size based on your layout
+            notePanel.Name = "notePanel";
+            panel2.Controls.Add(notePanel);
+
+            int buttonTop = 0; // Adjust the top position based on your layout
+
+            foreach (string noteName in noteNames)
+            {
+                Button noteButton = new Button();
+                noteButton.BackColor = SystemColors.Control;
+                noteButton.Location = new Point(6, buttonTop);
+                noteButton.Name = "dynamicNoteButton";
+                noteButton.Size = new Size(500, 103);
+                noteButton.TabIndex = 0;
+                noteButton.Text = noteName;
+                noteButton.UseVisualStyleBackColor = false;
+                noteButton.Click += DynamicNoteButton_Click;
+
+                Button deleteButton = new Button();
+                deleteButton.BackColor = SystemColors.ControlLight;
+                deleteButton.Location = new Point(400, buttonTop);
+                deleteButton.Name = "dynamicDeleteButton";
+                deleteButton.Size = new Size(100, 103);
+                deleteButton.TabIndex = 1;
+                deleteButton.Text = "Delete";
+                deleteButton.UseVisualStyleBackColor = false;
+                deleteButton.Click += (sender, e) => DeleteNoteButton_Click(noteName);
+
+                // Add the button to your panel (Assuming you have a panel named panel2)
+                notePanel1.Controls.Add(noteButton);
+                notePanel.Controls.Add(deleteButton);
+
+                // Adjust the top position for the next button
+                buttonTop += noteButton.Height + 8; // Add some spacing between buttons
+            }
+        }
+
+        private bool NoteExistsInDatabase(string noteName, MySqlConnection connection)
+        {
+            using (MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*) FROM Notes WHERE NoteName = @NoteName", connection))
+            {
+                cmd.Parameters.AddWithValue("@NoteName", noteName);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count > 0;
+            }
+        }
+
+        private void DynamicNoteButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string selectedNoteName = clickedButton.Text;
+            string noteText = GetNoteTextFromDatabase(selectedNoteName);
+
+            // Set the selected note name to the TextBox
+            noteNameTextBox.Text = selectedNoteName;
+
+            // Assuming you have RichTextBoxes named noteName and noteSpace
+            noteSpace.Text = noteText;
+        }
+
+        private void ClearNoteButtons()
+        {
+            // Clear existing note buttons from the panel
+            foreach (Control control in panel2.Controls)
+            {
+                if (control.Name == "dynamicNoteButton")
+                {
+                    panel2.Controls.Remove(control);
+                }
+            }
+        }
+
+        private void DeleteNoteButton_Click(string noteName)
+        {
+            try
+            {
+                // Delete the note from the database
+                DeleteFromDatabase(noteName);
+
+                MessageBox.Show("Note deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DisplayNoteNames();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DeleteFromDatabase(string noteName)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("DELETE FROM Notes WHERE NoteName = @NoteName", connection))
+                {
+                    cmd.Parameters.AddWithValue("@NoteName", noteName);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayNoteNames();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Code for button2 click event goes here
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Code for button3 click event goes here
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            // Code for panel3 paint event goes here
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Code for richTextBox1 text changed event goes here
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void newBtn_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
